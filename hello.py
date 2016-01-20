f67733e44a060085d9ed227edbb2d1f6cfe0aa39
Jeremy Zhang <jeremyzhang5000@gmail.com>
2016-01-19 17:53:24 -0800

    Update hello.py
    Usernames

--- hello.py
+++ hello.py
@@ -2,9 +2,13 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route('/<username>')
+def show_user_profile(username):
+    return "Hello " + username + "!"
 
 if __name__ == "__main__":
     app.debug = True
