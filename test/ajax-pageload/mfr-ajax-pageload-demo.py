a8f475b38a8e83150f3f9455351272da803b84ac
Jeremy Zhang <jeremyzhang5000@gmail.com>
2016-01-23 00:04:18 -0800

    Update ajax-pageload-demo
    Here you go, the footer. And also updated the python because it is not
    properly getting the details from iceboats server.

--- test/ajax-pageload/mfr-ajax-pageload-demo.py
+++ test/ajax-pageload/mfr-ajax-pageload-demo.py
@@ -5,31 +5,38 @@ import urllib2
 
 app = Flask(__name__)
 
-response = urllib2.urlopen('http://radio.mane-frame.com/status-json.xsl')
-xsl = response.read()
-mfr_json = json.loads(xsl)
-mfr_json = mfr_json["icestats"]["source"]
-mfr_json.pop()
-mfr_json = mfr_json.pop()
+def getPlaying():
+    response = urllib2.urlopen('http://radio.mane-frame.com/status-json.xsl')
+    xsl = response.read()
+    mfr_json = json.loads(xsl)
+    mfr_json = mfr_json["icestats"]["source"]
+    mfr_json.pop()
+    mfr_json = mfr_json.pop()
+    return mfr_json
 
 @app.route('/')
 def index():  
+    mfr_json = getPlaying()
     return render_template('header.html', title='index') + render_template('menu.html', menubarhomeactive='active') + render_template('player.html', playing=str(mfr_json["title"]), viewers=mfr_json["listeners"]) + render_template('footer.html')
 
 @app.route('/content1')
 def content1():
+    mfr_json = getPlaying()
     return render_template('header.html', title='content1') + render_template('menu.html', menubarcontent1active='active') + render_template('content1.html') + render_template('player.html', playing=str(mfr_json["title"]), viewers=mfr_json["listeners"]) + render_template('footer.html')
 
 @app.route('/content2')
 def content2():
+    mfr_json = getPlaying()
     return render_template('header.html', title='content2') + render_template('menu.html', menubarcontent2active='active') + render_template('content2.html') + render_template('player.html', playing=str(mfr_json["title"]), viewers=mfr_json["listeners"]) + render_template('footer.html')
 
 @app.route('/content3')
 def content3():
+    mfr_json = getPlaying()
     return render_template('header.html', title='content3') + render_template('menu.html', menubarcontent3active='active') + render_template('content3.html') + render_template('player.html', playing=str(mfr_json["title"]), viewers=mfr_json["listeners"]) + render_template('footer.html')
 
 @app.route('/update_radio_subtxt')
 def update_radio_subtxt():
+    mfr_json = getPlaying()
     return "<strong>Currently playing: </strong>" + str(mfr_json["title"]) + " - <strong>Currently viewing: </strong>" + str(mfr_json["listeners"])
     #return "test"
 
