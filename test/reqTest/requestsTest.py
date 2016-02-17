d20fb8d42a3455dec1640e7c030723cc71f69958
Jeremy Zhang <jeremyzhang5000@gmail.com>
2016-02-17 11:14:04 -0800

    Added requests system and updated about.html
    Added the requests system! Also, updated the about page to include the
    paragraph written by Vapid Pixel, THANKS!

--- test/reqTest/requestsTest.py
+++ test/reqTest/requestsTest.py
@@ -1,82 +1,15 @@
 from flask import Flask, render_template, request, jsonify
-from xml2dict import ElementTree, XmlListConfig, XmlDictConfig
-from crossdomainfuncs import crossdomain
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.paginate import Pagination
 from sqlalchemy import *
-import urllib2
 import json
-import commands
 import os
 import MySQLdb
 import time
 import datetime
 import math
-import re
-
 app = Flask(__name__)
-gitRevision = commands.getoutput("git rev-parse --short master")
 
-def getMFRTumblr():
-    response = urllib2.urlopen('http://maneframeradio.tumblr.com/api/read?type=post&start=0&num=10')
-    html = response.read()
-    root = ElementTree.XML(html)
-    xmldict = XmlDictConfig(root)
-    xmldict = xmldict["posts"]
-    x = xmldict["post"]
-    return x
-
-
-@app.route('/')
-def index():
-    t = getMFRTumblr()
-    t = t[0]
-    postTitle = t["regular-title"]
-    postContent = t["regular-body"]
-    postURL = t["url"]
-    postTimestamp = t["date"]
-    return render_template('header.html', title="Welcome") + render_template('menu.html', animatedMenubar="animated fadeInDown") + render_template('index.html', postTitle = postTitle, postContent = postContent, postURL = postURL, postTimestamp = postTimestamp) + render_template('sidebar.html', revision=gitRevision) + render_template('player.html') + render_template('footer.html')
-
-@app.route('/about')
-def about():
-    return render_template('header.html', title="About") + render_template('menu.html', activeAbout="active") + render_template('about.html') + render_template('sidebar.html', revision=gitRevision) + render_template('player.html') + render_template('footer.html')
-
-@app.route('/blog')
-def blog():
-    t = getMFRTumblr()
-    posts = []
-    for x in t:
-        a = {'title': x["regular-title"], 'url': x["url"], 'timestamp': x["date"], 'content': x["regular-body"]}
-        posts.append(a)
-    return render_template('header.html', title="Blog") + render_template('menu.html', activeBlog="active") + render_template('blog.html', posts=posts) + render_template('sidebar.html', revision=gitRevision) + render_template('player.html') + render_template('footer.html')
-
-@app.route('/events')
-def events():
-    return render_template('header.html', title="Events") + render_template('menu.html', activeEvents="active") + render_template('events.html') + render_template('sidebar.html', revision=gitRevision) + render_template('player.html') + render_template('footer.html')
-
-@app.route('/team')
-def team():
-    return render_template('header.html', title="Team") + render_template('menu.html', activeTeam="active") + render_template('team.html') + render_template('sidebar.html', revision=gitRevision) + render_template('player.html') + render_template('footer.html')
-
-@app.route('/donate')
-def donate():
-    return render_template('header.html', title="Donate") + render_template('menu.html', activeDonate="active") + render_template('donate.html') + render_template('sidebar.html', revision=gitRevision) + render_template('player.html') + render_template('footer.html')
-
-@app.route('/contact')
-def contact():
-    return render_template('header.html', title="Contact") + render_template('menu.html', activeContact="active") + render_template('contact.html') + render_template('sidebar.html', revision=gitRevision) + render_template('player.html') + render_template('footer.html')
-
-@app.route('/update_radio_subtxt', methods=['GET', 'OPTIONS'])
-@crossdomain(origin='*')
-def update_radio_subtxt():
-    response = urllib2.urlopen('http://radio.mane-frame.com/status-json.xsl')
-    xsl = response.read()
-    mfr_json = json.loads(xsl)
-    mfr_json = mfr_json["icestats"]["source"]
-    mfr_json.pop()
-    mfr_json = mfr_json.pop()
-    return str(mfr_json["title"]) + " <em>" + str(mfr_json["listeners"]) + "</em>"
-
-#Begin Requests system
 def file_get_contents(filename):
     with open(os.path.dirname(os.path.realpath(__file__)) + "/" + filename) as f:
         return f.read()
@@ -132,9 +65,9 @@ def request_post():
                             if addr not in trusted_proxies), request.remote_addr)
 
         reqSONGID = MySQLdb.escape_string(str(request.form['reqSONGID']))
-        reqUSERNAME = MySQLdb.escape_string(str(request.form['reqUSERNAME'])).replace("%", "")
+        reqUSERNAME = MySQLdb.escape_string(str(request.form['reqUSERNAME']))
         reqIP = str(remote_addr)
-        reqMSG = MySQLdb.escape_string(str(request.form['reqMSG'])).replace("%", "")
+        reqMSG = MySQLdb.escape_string(str(request.form['reqMSG']))
         reqTIMESTAMP = str(datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S'))
         t = connection.execute("SELECT * FROM songs LIMIT 1 OFFSET " + str(int(reqSONGID) - 1))
         reqID = ""
@@ -147,8 +80,7 @@ def request_post():
         connection.execute("""INSERT INTO `requests` (`songID`, `username`, `userIP`, `message`, `requested`) VALUES (""" + str(reqSONGID) + """, '""" + reqUSERNAME + """', '""" + reqIP + """', '""" + reqMSG + """', '""" + reqTIMESTAMP + """');""")
         return render_template('requestConfirmation.html', reqID=reqSONGID, reqTITLE=reqTITLE, reqARTIST=reqARTIST, reqUSERNAME=reqUSERNAME)
     return "GET IS NOT SUPPORTED ON /request-post"
-#End Requests System
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.debug = True
     app.run(host= '0.0.0.0')
